a6fb778d21ad7f3d3f29ba5101d4b597bd407885
kaydenwilliams <kaydenwilliams@11.25.5.31>
2025-01-30 02:15:43 -0600

    given base code/instructions

--- src/program01/Program01.java
+++ src/program01/Program01.java
@@ -1,5 +1,51 @@
 package program01;
 
 public class Program01 {
-
+	
+	public static void main(String[] args) {
+		
+		public static void initDeck(boolean[] deck)
+		// set the values of deck to indicate that they are all
+		// present - not dealt yet.
+		public static boolean emptyDeck(boolean[] deck)
+		// returns whether or not all the cards in the deck
+		// have already been dealt.
+		public static int dealCard(boolean[] deck)
+		// returns a card (an int in the range 0 to 51) at random
+		// that has not been dealt since the deck was initialize
+		// via intDeck. Also notes (in deck) that this card is
+		// no longer available.
+		public static void printCard(int card)
+		// given a card (an int in the range 0 to 51) prints
+		// an appropriate representation of this card based
+		// on a 1-1 and onto mapping of the set [0, 51] to
+		// the cards described above.
+		
+		
+		boolean[] myDeck = new boolean[52];
+		final int cardsPerRow = 8;
+		int cardsThisRow = 0;
+		int myCard;
+		
+		initDeck(myDeck);
+		
+		System.out.println("\nHere is a shuffled deck ...\n");
+		
+		while (!emptyDeck(myDeck)) {
+			myCard = dealCard(myDeck);
+			++cardsThisRow;
+			if (cardsThisRow <= cardsPerRow) {
+				printCard(myCard);
+				System.out.print(" ");
+				}
+			else {
+				System.out.println("");
+				cardsThisRow = 1;
+				printCard(myCard);
+				System.out.print(" "); 
+				}
+			}
+		System.out.println("\n");
+	
+	}
 }
